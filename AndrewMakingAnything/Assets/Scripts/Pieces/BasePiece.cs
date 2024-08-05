9f249717d975ee324c05f7f5483d0d525f0b8274
Philippe Vinchon <p.vinchon@gmail.com>
2024-08-05 21:21:06 +0100

    How to make 2D Chess in Unity - Piece Events (Pt. 3)

--- AndrewMakingAnything/Assets/Scripts/Pieces/BasePiece.cs
+++ AndrewMakingAnything/Assets/Scripts/Pieces/BasePiece.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -12,6 +13,9 @@ public class BasePiece : EventTrigger
 
     protected RectTransform mRectTransform = null;
     protected PieceManager mPieceManager;
+
+    protected Vector3Int mMovement = Vector3Int.one;
+    protected List<Cell> mHighlightedCells = new List<Cell>();
 
     public virtual void Setup(Color newTeamColor, Color32 newSpriteColor, PieceManager newPieceManager)
     {
@@ -32,4 +36,72 @@ public class BasePiece : EventTrigger
         gameObject.SetActive(true);
     }
 
+    private void CreateCellPath(int xDirection, int yDirection, int movement)
+    {
+        int currentX = mOriginalCell.mBoardPosition.x;
+        int currentY = mOriginalCell.mBoardPosition.y;
+
+        for (int i = 1; i <= movement; i++)
+        {
+            currentX += xDirection;
+            currentY += yDirection;
+
+            mHighlightedCells.Add(mOriginalCell.mBoard.mAllCells[currentX, currentY]);
+        }
+    }
+
+    protected virtual void CheckPathing()
+    {
+        CreateCellPath(1, 0, mMovement.x);
+        CreateCellPath(-1, 0, mMovement.x);
+
+        CreateCellPath(0, 1, mMovement.y);
+        CreateCellPath(0, -1, mMovement.y);
+
+        CreateCellPath(1, 1, mMovement.z);
+        CreateCellPath(-1, 1, mMovement.z);
+
+        CreateCellPath(-1, -1, mMovement.z);
+        CreateCellPath(1, -1, mMovement.z);
+    }
+
+    protected void ShowCells()
+    {
+        foreach (Cell cell in mHighlightedCells)
+        {
+            cell.mOutlineImage.enabled = true;
+        }
+    }
+
+    protected void ClearCells()
+    {
+        foreach (Cell cell in mHighlightedCells)
+        {
+            cell.mOutlineImage.enabled = false;
+        }
+
+        mHighlightedCells.Clear();
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+
+        CheckPathing();
+        ShowCells();
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+
+        transform.position += (Vector3)eventData.delta;
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+
+        ClearCells();
+    }
 }
