556159f69768f7171988e774d10acf0633dd293f
Philippe Vinchon <p.vinchon@gmail.com>
2024-08-08 13:10:47 +0100

    How to make 2D Chess in Unity - Finishing Up (Pt. 7)

--- AndrewMakingAnything/Assets/Scripts/Pieces/Knight.cs
+++ AndrewMakingAnything/Assets/Scripts/Pieces/Knight.cs
@@ -9,4 +9,34 @@ public class Knight : BasePiece
 
         GetComponent<Image>().sprite = Resources.Load<Sprite>("T_Knight");
     }
+
+    private void CreateCellPath(int flipper)
+    {
+        int currentX = mCurrentCell.mBoardPosition.x;
+        int currentY = mCurrentCell.mBoardPosition.y;
+
+        MatchesState(currentX + 2, currentY + 1 * flipper);
+        MatchesState(currentX + 1, currentY + 2 * flipper);
+        MatchesState(currentX - 1, currentY + 2 * flipper);
+        MatchesState(currentX - 2, currentY + 1 * flipper);
+    }
+
+    protected override void CheckPathing()
+    {
+        CreateCellPath(1);
+        CreateCellPath(-1);
+    }
+
+    private void MatchesState(int targetX, int targetY)
+    {
+        CellState cellState = mCurrentCell.mBoard.ValidateCell(targetX, targetY, this);
+
+        switch (cellState)
+        {
+            case CellState.Free:
+            case CellState.Enemy:
+                mHighlightedCells.Add(mCurrentCell.mBoard.mAllCells[targetX, targetY]);
+                break;
+        }
+    }
 }
